493c09d5b1217f5b6526a9cf11141f6edf705c6e
Andy-CE17 <andy.campos@tecsup.edu.pe>
2026-03-31 14:22:31 -0500

    update controller responsable

--- src/main/java/pe/edu/tecsup/lab03/controllers/StudentController.java
+++ src/main/java/pe/edu/tecsup/lab03/controllers/StudentController.java
@@ -2,6 +2,6 @@ package pe.edu.tecsup.lab03.controllers;
 
 public class StudentController {
     public void test() {
-        System.out.println("Responsable");
+        System.out.println("ok");
     }
 }
