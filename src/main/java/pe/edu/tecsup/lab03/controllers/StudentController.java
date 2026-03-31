117be508bf3fea4d10d97fa151f22db145fe7a6f
Andy-CE17 <andy.campos@tecsup.edu.pe>
2026-03-31 15:02:43 -0500

    update controller sprint-2 responsable

--- src/main/java/pe/edu/tecsup/lab03/controllers/StudentController.java
+++ src/main/java/pe/edu/tecsup/lab03/controllers/StudentController.java
@@ -4,7 +4,12 @@ public class StudentController {
     public void test() {
         System.out.println("Responsable");
     }
+
     public void colaborador() {
         System.out.println("Responsable - 2");
+    }
+
+    public void responsableSprint2() {
+        System.out.println("Responsable sprint-2");
     }
 }
